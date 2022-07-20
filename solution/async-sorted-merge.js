0835f4a35b5870934765053595a0a737e81b32ff
siamhash <100968553+siamhash@users.noreply.github.com>
2022-07-20 18:27:18 +0500

    Update async-sorted-merge.js

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -23,7 +23,6 @@ module.exports = (logSources, printer) => {
     if (entries.length < 25) {
       entries = require("../utils/general").insertionSortByDate(entries);
     } else {
-      // entries = require("../utils/general").quickSortByDate(entries, 0, entries.length - 1);
       entries.sort((a, b) => {  // must sort all entries
         var c = new Date(a.date);
         var d = new Date(b.date);
